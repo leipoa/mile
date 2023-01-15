11a3a2c7de10c027921ea76e18763890e0663a33
Darya Vylomova <qwerty6108@gmail.com>
2023-01-15 17:25:59 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int r = 4000;
+        int m = r / 20;
+
+        System.out.println( "Количество миль =" +  m );
+    }
+}
