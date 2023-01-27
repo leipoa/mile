3ec4c8075d071478365da2139cf91690d38d8a4d
Darya Vylomova <qwerty6108@gmail.com>
2023-01-27 13:29:12 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+
+    public int calculate(int prise) {
+        int bonus = prise / 20;
+        return bonus;
+
+
+    }
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 4000;
-        int mile = price / 20;
-
-        System.out.println( "Количество миль =" +  mile );
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
+
+
 }
